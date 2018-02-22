6c7e94411fe61546c3f0e3bb10d1b31238080408
zhaosec <haoquan.zhao@gmail.com>
2018-02-21 22:50:24 -0500

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class Dice {
 	   int numberShowing;
 	    public int roll() {
-	        Random random = new Random(); /* <-- this is a constructor */
-	        numberShowing = random.nextInt(6) + 1; /* <-- look at the API doc for nextInt() to see why we give it 6 as and argument, and why we need to add 1 to the result */
+	        Random random = new Random();
+	        numberShowing = random.nextInt(6) + 1; 
 	        return numberShowing;
 	        
 	    }

9af1ad2ba1f2c80bcbd533e87909a49566db6758
zhaosec <haoquan.zhao@gmail.com>
2018-02-21 23:39:26 -0500

    Add files via upload

--- Dice.java
+++ Dice.java
@@ -1,14 +1,31 @@
 package dice;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import com.sun.prism.Graphics;
+
 public class Dice {
-	   int numberShowing;
+	private BufferedImage die1;
+	private BufferedImage die2;
+	private BufferedImage die3;
+	private BufferedImage die4;
+	private BufferedImage die5;
+	private BufferedImage die6;
+	   int DiceNumber;
 	    public int roll() {
-	        Random random = new Random();
-	        numberShowing = random.nextInt(6) + 1; 
-	        return numberShowing;
-	        
+	        Random random = new Random(); 
+	        DiceNumber = random.nextInt(6) + 1; 
+	        return DiceNumber;
+	    }
+	    public void DiceRoll(JLabel dieLabel){
+	    dieLabel.setIcon(new ImageIcon("die" + DiceNumber + ".png"));
 	    }
 }
 	   
